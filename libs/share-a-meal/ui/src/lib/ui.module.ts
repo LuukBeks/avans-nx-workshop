f99b7abd8b21f5116b35c2a4749b2eb192346129
Luuk Beks <beksluuk@gmail.com>
2023-11-09 09:44:12 +0100

    added header and about page

--- libs/share-a-meal/ui/src/lib/ui.module.ts
+++ libs/share-a-meal/ui/src/lib/ui.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './info/about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AboutComponent, HeaderComponent],
+  exports: [AboutComponent, HeaderComponent],
 })
 export class UiModule {}
